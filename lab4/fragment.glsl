9e19d4e7194575d21fddc85a55496d364fadc33b
Austin Tung <austin.tung56@gmail.com>
2019-02-06 01:12:04 +0000

    ok

--- lab4/fragment.glsl
+++ lab4/fragment.glsl
@@ -9,6 +9,21 @@ void main()
     vec4 specular = vec4(0, 0, 1, 1);
     
     //ambient
+     // Ia = Ra * La     0 <= Ra <= 1
     ambient = gl_LightSource[0].ambient * gl_LightModel.ambient * gl_FrontMaterial.ambient;
+    
+    //diffuse
+    // Id = Rd * Ld * max(dot(n,l),0)
+    vec3 n = normalize(normal);
+    vec3 l = normalize(light_direction);
+    diffuse += gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse * max(dot(n,l),0);
+    
+    //specular
+    // Is = Rs * Ls * max(dot(v,r),0)^alpha
+    vec4 e = normalize(-position);
+    vec3 r = normalize((2*dot(n,l)) * n - l);
+    specular += gl_FrontMaterial.specular * gl_LightSource[0].specular * pow(max(dot(e,r),0),gl_FrontMaterial.shininess);
+    
+    
     gl_FragColor = ambient + diffuse + specular;
 }
